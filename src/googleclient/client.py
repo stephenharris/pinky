b48c9d19cf170e8572c22dda32539823f03079a5
Stephen Harris <contact@stephenharris.info>
2025-11-25 23:04:02 +0000

    use offline

--- src/googleclient/client.py
+++ src/googleclient/client.py
@@ -24,7 +24,7 @@ def authenticate():
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
+            creds = flow.run_local_server(port=0,prompt='consent',access_type='offline')
         # Save token for next time
         with open('token.json', 'w') as token:
             token.write(creds.to_json())
