d9fb9b1c32686f227877f85441e5f5f81ab0cdfd
Stephen Harris <contact@stephenharris.info>
2025-12-02 21:00:02 +0000

    add debug logging

--- src/googleclient/client.py
+++ src/googleclient/client.py
@@ -18,17 +18,24 @@ def authenticate():
 
     if os.path.exists('token.json'):
         creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+        creds_json = creds.to_json()
+        logging.info(f"Using credentials from token.json, expires {creds.expiry}")
     
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
+            logging.info(f"Refreshed credentials, expires {creds.expiry}")
         else:
+            if not creds:
+                logging.info(f"Token: {creds.to_json()}")
+            else:
+                logging.info("no token")
             flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0,prompt='consent',access_type='offline')
+            creds = flow.run_local_server(port=0,access_type='offline')
         # Save token for next time
         with open('token.json', 'w') as token:
-            token.write(creds.to_json())
-        
-        logging.info("Saved credentials to token.json")
+            creds_json = creds.to_json()
+            token.write(creds_json)
+            logging.info(f"Saved credentials to token.json {creds_json.get('expiry')}")
 
     return creds
